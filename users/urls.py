d5b9333563fd6e602076444ec9f3603de8ae71c7
Darshan A S <itisdarshan@gmail.com>
2019-11-25 01:44:36 +0530

    Remove direct APIs for Address

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 
+from users.apis.borrower_api import BorrowerViewSet
 from users.apis.lender_api import LenderViewSet
 
 router = routers.DefaultRouter()
 router.register('api/lenders', LenderViewSet, 'lenders')
+router.register('api/borrowers', BorrowerViewSet, 'borrowers')
 
 urlpatterns = router.urls
